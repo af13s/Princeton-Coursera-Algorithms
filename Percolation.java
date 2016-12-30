1eac8886cee83d256098e6f5c5e95914c1c425fd
af13s <af13s@my.fsu.edu>
2016-12-30 13:12:00 -0500

    Updated Code
    First error test attempt

--- Percolation.java
+++ Percolation.java
@@ -1,63 +1,92 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation
 {
 
-  int id[][];
-  boolean vsite;
+  boolean id[];
+  int  vsite1, vsite2;
   int num=0;
-  WeightedQuickUnionUF(int n);
+  WeightedQuickUnionUF wquf;
 
   // object current should then connect neighbor sites
   // objet should calculate if neighbor sites are filled, fill current site.
-  //add error handling warning
 
   public Percolation(int n) // create n-by-n grid, with all sites blocked
   {
     num = n;
-    id = new int [n][n];
 
-    for(int i =0; i < n; i++)
-      for (j=0;j<n;j++)
-        id[i][j] =-1;
+    id = new boolean [n*n]; // store what site are open
+    wquf = new wquf ((n*n)+2); // see what sites are connected
+
+    vsite1 = (n*n);
+    vsite2 = (n*n)+1;
+
+    for (int i =0; i < (n*n); i++)
+      id[i] = false;
   }
 
   public void open(int row, int col) // open site (row, col) if it is not open already
   {
     if (row || col <=0)
-      throw new java.lang.IndexOutOfBoundsException("Row and Colummn must be between greater than 0");
+      exception();
 
-    else if (row ==1)
-      id[row-1][col-1] = 1;
-    else
-      id[row-1][col-1] = 0
+    int position = (num * (row-1)) + (col-1);
 
-      if (if (row-1 == num) && isFull(row,col))
-        if vsite = true;
+    id[position] = true;
+
+    //connect to open neighbors
+
+    //row above
+    if  (isOpen(row-1,col) && (row > 1))
+    wquf.union(postion-n,position);
+    //row below
+    if  (isOpen(row+1,col) && (row < num))
+    wquf.union(postion+n,position);
+    //col to left
+    if  (isOpen(row,col-1) && (col > 1))
+    wquf.union(postion-1,position);
+    //col to right
+    if  (isOpen(row,col+1) && (col < n))
+    wquf.union(postion+1,position);
+
+    // Connect beginning virtual site if any site in the first row is open
+    if (row == 1)
+      wquf.union(num * (row-1)) + (col-1), vsite1);
+
+    //Connect ending virtual site if any of the site in the last row are open
+    if (row = num)
+      wquf.union ((num * (row-1)) + (col-1),vsite2);
   }
 
   public boolean isOpen(int row, int col) // is site (row, col) open?
   {
     if (row || col <=0)
-      throw new java.lang.IndexOutOfBoundsException("Row and Colummn must be between greater than 0");
+      exception();
 
-    return (id[row-1][col-1] > -1)
+    return (id[(num * (row-1)) + (col-1)]);
   }
 
   public boolean isFull(int row, int col) // is site (row, col) full?
   {
     if (row || col <=0)
-      throw new java.lang.IndexOutOfBoundsException("Row and Colummn must be between greater than 0");
+      exception();
 
-    return (id[row-1][col-1] > 0)
+    return (id[(num * (row-1))] + (col-1)] && wquf.connected(vsite1,(num * (row-1))] + (col-1));
   }
 
   public boolean percolates() // does the system percolate?
   {
-    return vsite;
+    return (wquf.connected(vsite1,vsite2));
   }
 
   public static void main(String[] args) // test client (optional)
   {
 
+  }
+
+  private void exception()
+  {
+    throw new java.lang.IndexOutOfBoundsException("Row and Colummn must be between greater than 0");
   }
 
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,14 +1,74 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import percolation;
+
 public class PercolationStats
 {
+  int results[];
+  int num =0;
+
    public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
+   {
+     num = trials;
+     results = new double[trials];
+
+     for (int i =0; i < trials; i++)
+     {
+       results[i] = trial(n);
+     }
+   }
+
+   private double trial(int n)
+   {
+     percolation trial (n);
+
+     for (int i =0; i < n; i++)
+     {
+       trial.open(uniform(1,n), uniform(1,n));
+       if (trial.percolates())
+        return (i/(n*n));
+     }
+
+   }
 
    public double mean()                          // sample mean of percolation threshold
+   {
+     return mean(results);
+   }
 
    public double stddev()                        // sample standard deviation of percolation threshold
+   {
+     return stddev(results);
+   }
 
    public double confidenceLo()                  // low  endpoint of 95% confidence interval
+   {
+     return (mean() - ((1.96 * stddev()) / Math.sqrt(num));
+   }
 
    public double confidenceHi()                  // high endpoint of 95% confidence interval
+   {
+     return (mean() + ((1.96 * stddev()) / Math.sqrt(num));
+   }
 
    public static void main(String[] args)        // test client (described below)
+   {
+     //include a main() method that takes two command-line arguments n and T, performs T independent computational experiments (discussed above) on an n-by-n grid
+     //and prints the sample mean, sample standard deviation, and the 95% confidence interval for the percolation threshold.
+
+    Scanner reader = new Scanner(System.in); // Reading from System.in
+
+    System.out.println("java PercolationStats ");
+    int n = reader.nextInt(); // Scans the next token of the input as an int.
+    int trials = reader.nextInt(); // Scans the next token of the input as an int.
+
+    PercolationStats result (n, trials);
+
+    System.out.println("Mean                    = " , result.mean());
+    System.out.println("Stddev                  = " , result.stddev());
+    System.out.println("95% confidence interval = " , result.confidenceLo(), " " , result.confidenceHi());
+
+   }
 }
